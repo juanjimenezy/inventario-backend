0ff954cb14522598563641a9c18a54688b644425
juanjimenezy <juanjiimenez8@gmail.com>
2022-03-02 21:16:36 -0500

    cargo

--- src/main/java/com/nexos/admision/controllers/CargoController.java
+++ src/main/java/com/nexos/admision/controllers/CargoController.java
@@ -1,8 +1,12 @@
 package com.nexos.admision.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,19 +27,26 @@ public class CargoController {
 	private CargoService cargoService;
 	
 	@GetMapping("/cargos")
-	public List<Cargo> listCargo(){
-		
-		return cargoService.findAll();
+	public ResponseEntity<?> listCargo(){
+		Map<String, Object> response = new HashMap<>();
+		List<Cargo> cargos = cargoService.findAll();
+		if(cargos.size() == 0) {
+			response.put("mensaje", "No existen cargos");
+			return new ResponseEntity<Map<String, Object>>(response,HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<List<Cargo>>(cargos,HttpStatus.OK);
 	}
 	
 	@GetMapping("/cargosById")
-	public Cargo cargo(@RequestParam Long id){
-		return cargoService.findById(id);
+	public ResponseEntity<?> cargo(@RequestParam Long id){
+		Cargo cargo = cargoService.findById(id);
+		return new ResponseEntity<Cargo>(cargo,HttpStatus.OK);
 	}
 	
 	@PostMapping("/cargos")
-	public Cargo created(@RequestBody Cargo cargo) {
-		return cargoService.save(cargo);
+	public ResponseEntity<?> created(@RequestBody Cargo cargo) {
+		Cargo cargoNew = cargoService.save(cargo);
+		return new ResponseEntity<Cargo>(cargoNew,HttpStatus.OK);
 	}
 
 }
